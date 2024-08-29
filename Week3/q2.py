a5ebe40b038ea3b4983bb845ecab348664e55046
HIMANSHU RAJORA <156173522+himanshurjora@users.noreply.github.com>
2024-08-29 16:02:31 +0530

    q2.py

--- Week3/q2.py
+++ Week3/q2.py
@@ -1,14 +1,15 @@
-
+n=int(input("enter "))
 num = 1
-for i in range(5):
+for i in range(n):
     for j in range(num):
         print("*", end =" ")
     num += 1
     print("")
 
-num1 = 4
-for i in range(4):
-    for j in range(num1):
+num1 = num-1
+for i in range(num1):
+    for j in range(num1-1):
         print("*", end =" ")
     num1 -= 1
     print("")
+4
